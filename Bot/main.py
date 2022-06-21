6632c3b1b11eb592363202272a20096d3650176e
LuizLourenco <luiz.lourenco@outlook.com.br>
2022-06-21 10:23:32 -0400

    atualizando webhook

--- Bot/main.py
+++ Bot/main.py
@@ -3,24 +3,27 @@
 ## e assim trazer dados da DF para webhook processar dados esperciais ou registralos em banco de dados.
 #######################################################################################
 
+#Importando bibliotacas 
+from pickle import TRUE
 from flask import Flask, request, jsonify
 
+#instanciando o objeto com as bibliotecas flask
 app = Flask(__name__)
 
+#pedidos = []
 
-pedidos = []
+#instanciando a chamada da qual endereco sera conhecido e seu metodo de comunicacao
+@app.route('/webhook', methods=['POST'])
+def main(): 
+    dfData = request.get_json(silent=True)
+    contexts = dfData['queryResult']['outputContexts'] # coletando as interações e seus contextos do DF na varial contexts
 
-@app.route('/', methods=['POST'])
-def main():
-    data = request.get_json(silent=True)
-
-    contexts = data['queryResult']['outputContexts']
-    for context in contexts:
-        if 'pizzapedido-followup' in context['name']:
-            parametros = context['parameters']
-            sabor = parametros['sabor']
-            nome = parametros['nome']['name']
-            #pedidos.append({ 'nome': nome, 'sabor': sabor })
+    for context in contexts: 
+        #if 'pizzapedido-followup' in context['name']:
+        #    parametros = context['parameters']
+        #    sabor = parametros['sabor']
+        #    nome = parametros['nome']['name']
+        #    #pedidos.append({ 'nome': nome, 'sabor': sabor })
 
     print(dfData)
     #print(pedidos)
@@ -29,10 +32,8 @@ def main():
     #Essa mensagem será apresentada ao usuário
     dfData['fulfillmentText'] = 'Essa e uma msg do webhook'
     return jsonify(dfData)
-    
-    return jsonify(data)
+
 
 # run Flask app
 if __name__ == "__main__":
-    app.debug = False
-    app.run()
+    app.run(Debug=TRUE)
